a4a617a9738dddc44c66477d3b5bcc7f06b58cca
javierbyte <hola@javierbyte.com>
2014-05-24 02:43:18 -0600

    added specific events

--- www/code.js
+++ www/code.js
@@ -8,3 +8,7 @@ $('*[data-progress]').each(function(){
 	theProgress = $(this).data('progress');
 	$(this).append('<div class="advance" style="width:' + theProgress + '%"></div>');
 });
+
+$('*[data-section="evento"]').on('click', function() {
+	$('#eventoPoster').attr('src', $(this).find('img').attr('src') );
+});
