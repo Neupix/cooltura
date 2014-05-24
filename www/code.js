ddbe5b4b98367c612bbdc390abdc669edab3beba
javierbyte <hola@javierbyte.com>
2014-05-24 09:09:23 -0600

    added buy section, sinopsis, price in the button

--- www/code.js
+++ www/code.js
@@ -6,11 +6,18 @@ function onDeviceReady() {
 
 $('*[data-progress]').each(function(){
 	theProgress = $(this).data('progress');
+
 	$(this).append('<div class="advance" style="width:' + theProgress + '%"></div>');
+
+	if(theProgress < 33) $(this).find('.advance').css('background', '#d00');
+	if(theProgress == 100) $(this).find('.advance').css('background', '#06a');
 });
 
 $('*[data-section="evento"]').on('click', function() {
+	thePrice = $(this).data('precio');
+
 	$('#eventoPoster').attr('src', $(this).find('img').attr('src') );
 
+	$('#evento .price').html( '$' + thePrice );
 	$('#evento .title').html( $(this).data('title') );
 });
